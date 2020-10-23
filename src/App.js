25d378d28a94cd7934804821a59fb4b67335856b
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-22 18:13:33 -0700

    declared timeLeft variable and created an 'if' statement in calculateTimeLeft function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import React from "react";
 const calculateTimeLeft = () => {
   let year = new Date().getFullYear();
 
-  // + before new Date tells javascript to cast object as an integer
-  const difference = new Date(`10/01/${year}`) - +new Date();
+  // + before new Date tells javascript to cast object as an integer in microseconds
+  const difference = +new Date(`10/01/${year}`) - +new Date();
+
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    };
+  }
 };
 
 function App() {
