420faf61d33de0cb83413b57ba2d4274c32caab7
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-22 18:04:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const calculateTimeLeft = () => {
+  let year = new Date().getFullYear();
+
+  // + before new Date tells javascript to cast object as an integer
+  const difference = new Date(`10/01/${year}`) - +new Date();
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
